e3fb452437c957b5ff269c64a3c22b1104c121e8
hanhum001 <hanson.humphrey@fh-swf.de>
2023-05-23 18:42:40 +0200

    Chapter 2.

--- main.py
+++ main.py
@@ -51,7 +51,64 @@ def usingKneares():
     a = model.kneighbors_graph(x)
     print(a.toarray())
 
+def chaprterTwo():
+    from pathlib import Path
+    import numpy as np
+    import pandas as pd
+    import tarfile
+    import urllib.request
+    import matplotlib.pyplot as plt
+
+    # ⚠️ fetch the data.
+    def load_housing_data():
+        tarbal_path = Path("datasets/housing.tgz")
+        if not tarbal_path.is_file():
+            Path("datasets").mkdir(parents=True, exist_ok=True)
+            url = "https://github.com/ageron/data/raw/main/housing.tgz"
+            urllib.request.urlretrieve(url, tarbal_path)
+            with tarfile.open(tarbal_path) as housing_tarbal:
+                housing_tarbal.extractall(path="datasets")
+        return pd.read_csv(Path("datasets/housing/housing.csv"))
+
+    housing = load_housing_data()
+    # print(housing.info())
+    # a way to know the data within.
+    # print(housing["ocean_proximity"].value_counts(sort=True))
+    # print(housing.describe())
+    # housing.hist(bins=50, figsize=(12,8))
+    # plt.show()
+
+    # setting data aside
+
+    #def shuffle_and_split_data(data, test_ration):
+     #   shuffled_indeces = np.random.permutation(len(data))
+     #   test_set = int(len(data) * test_ration)
+      #  test_indices = shuffled_indeces[:test_set]
+      #  train_indices = shuffled_indeces[test_set:]
+       # return data.iloc[train_indices], data.iloc[test_indices]
+
+    #train_set, test_set = shuffle_and_split_data(housing, 0.2)
+    #print(len(train_set), len(test_set))
+
+    #from zlib import crc32
+    #def is_id_in_test_set(identifier, test_ratio):
+       # return crc32(np.int64(identifier)) < test_ratio * 2 ** 32
+
+   #def split_data_with_id_hash(data, test_ratio, id_column):
+        #ids = data[id_column]
+        #in_test_set = ids.apply(lambda id_:is_id_in_test_set(id_, test_ratio))
+        #return data.loc[~in_test_set], data.loc[in_test_set]
+
+    #housing_with_id = housing.reset_index()
+    #train_set, test_set = split_data_with_id_hash(housing_with_id, 0.2, "index")
+
+    #housing_with_id["id"] = housing["longitude"] * 1000 + housing["latitude"]
+    #train_set, test_set = split_data_with_id_hash(housing_with_id, 0.2, "id")
+
+    from sklearn.model_selection import train_test_split
+    train_set, test_set = train_test_split(housing, test_size= 0.2, random_state= 42)
+
+
 
 if __name__ == "__main__":
-    #checkyourself()
-    usingKneares()
+    chaprterTwo()
